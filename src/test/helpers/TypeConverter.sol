e464526830a664a28feac961a7035bfcb42e7fbd
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-11-15 16:40:52 +0000

    Use ActionID (#12)

--- src/test/helpers/TypeConverter.sol
+++ src/test/helpers/TypeConverter.sol
@@ -7,7 +7,7 @@ library TypeConverter {
         s[0] = "0";
         s[1] = "x";
         for (uint256 i = 0; i < 20; i++) {
-            bytes1 b = bytes1(uint8(uint256(uint160(x)) / (2**(8 * (19 - i)))));
+            bytes1 b = bytes1(uint8(uint256(uint160(x)) / (2 ** (8 * (19 - i)))));
             bytes1 hi = bytes1(uint8(b) / 16);
             bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
             s[2 * i + 2] = char(hi);
