f44e3d8b22ed3772318295855eeccfef935b6493
Miguel Piedrafita <github@miguelpiedrafita.com>
2023-06-02 01:41:52 +0200

    temp: remove tests (#25)
    * temp: remove tests
    
    * fix deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,72 +2,72 @@ import fs from "fs";
 import ora from "ora";
 import dotenv from "dotenv";
 import readline from "readline";
-import { Wallet } from "@ethersproject/wallet";
-import { hexlify, concat } from "@ethersproject/bytes";
-import { JsonRpcProvider } from "@ethersproject/providers";
-import { defaultAbiCoder as abi } from "@ethersproject/abi";
+import { polygonMumbai } from "viem/chains";
+import { privateKeyToAccount } from "viem/accounts";
+import { http, createWalletClient, createPublicClient } from "viem";
 dotenv.config();
 
 // make sure to fix the path if you've renamed your contract!
-const Contract = fs.readFileSync("./out/Contract.sol/Contract.json");
+const Contract = JSON.parse(
+	fs.readFileSync("./out/Contract.sol/Contract.json", "utf-8")
+);
 
 let validConfig = true;
 if (process.env.RPC_URL === undefined) {
-  console.log("Missing RPC_URL");
-  validConfig = false;
+	console.log("Missing RPC_URL");
+	validConfig = false;
 }
 if (process.env.PRIVATE_KEY === undefined) {
-  console.log("Missing PRIVATE_KEY");
-  validConfig = false;
+	console.log("Missing PRIVATE_KEY");
+	validConfig = false;
 }
 if (!validConfig) process.exit(1);
 
-const provider = new JsonRpcProvider(process.env.RPC_URL);
-const wallet = new Wallet(process.env.PRIVATE_KEY, provider);
+const wallet = createWalletClient({
+	transport: http(process.env.RPC_URL),
+	account: privateKeyToAccount(process.env.PRIVATE_KEY),
+});
+const client = createPublicClient({ transport: http(process.env.RPC_URL) });
 
 const ask = async (question) => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
 
-  return new Promise((resolve) => {
-    rl.question(question, (input) => {
-      resolve(input);
-      rl.close();
-    });
-  });
+	return new Promise((resolve) => {
+		rl.question(question, (input) => {
+			resolve(input);
+			rl.close();
+		});
+	});
 };
 
 async function main() {
-  const worldIDAddress = await fetch(
-    "https://developer.worldcoin.org/api/v1/contracts"
-  )
-    .then((res) => res.json())
-    .then(
-      (res) => res.find(({ key }) => key == "staging.semaphore.wld.eth").value
-    );
+	const worldIDAddress = await fetch(
+		"https://developer.worldcoin.org/api/v1/contracts"
+	)
+		.then((res) => res.json())
+		.then(
+			(res) => res.find(({ key }) => key == "staging.semaphore.wld.eth").value
+		);
 
-  let inputs = [];
-  // if you need any constructor parameters, use this to get them when running the script:
-  // inputs = [await ask("Param1: "), await ask("Param2: ")];
+	// if you need any extra constructor parameters, add them to this array in order
+	const inputs = [await ask("App ID: "), await ask("Action: ")];
 
-  const spinner = ora(`Deploying your contract...`).start();
+	const spinner = ora(`Deploying your contract...`).start();
 
-  let tx = await wallet.sendTransaction({
-    data: hexlify(
-      concat([
-        Contract.bytecode.object,
-        abi.encode(Contract.abi[0].inputs, [worldIDAddress, ...inputs]),
-      ])
-    ),
-    gasPrice: 60000000000,
-  });
+	const hash = await wallet.deployContract({
+		abi: Contract.abi,
+		chain: polygonMumbai,
+		args: [worldIDAddress, ...inputs],
+		bytecode: Contract.bytecode.object,
+	});
 
-  spinner.text = `Waiting for deploy transaction (tx: ${tx.hash})`;
-  tx = await tx.wait();
+	spinner.text = `Waiting for deploy transaction (tx: ${hash})`;
+	const tx = await client.waitForTransactionReceipt({ hash });
 
-  spinner.succeed(`Deployed your contract to ${tx.contractAddress}`);
+	spinner.succeed(`Deployed your contract to ${tx.contractAddress}`);
 }
 
 main(...process.argv.splice(2)).then(() => process.exit(0));
